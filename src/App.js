5fce9c5ae7945b9070aa41be87d8b3f98a6a2867
nathaneast <47707076+nathaneast@users.noreply.github.com>
2021-03-03 00:34:43 +0900

    Working: Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div>카테고리</div>
+      <div>카드리스트</div>
+      <div>푸터</div>
     </div>
   );
 }
